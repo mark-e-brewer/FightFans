2aee39cabd637826211bf909e52e1f17fa91496f
Mark Brewer <markbrewer369@gmail.com>
2023-09-15 21:39:34 -0500

    Class Creation and dbContext file setup.

---
+++ FightFansDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FightFans.Models;
+
+namespace FightFans
+{
+    public class FightFansDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public FightFansDbContext(DbContextOptions<FightFansDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿namespace FightFans.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public int? PostId { get; set; }
+
+        public int? UserId { get; set; }
+
+        public string? Content { get; set; }
+
+        public DateTime? createdOn { get; set; }
+        // Navigation Property (Many-to-One with Post)
+        public User? User { get; set; }
+        // Navigation Property (Many-to-One with User)
+        public Post? Post { get; set; }
+    }
+}
